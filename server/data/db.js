2bcadc59efcead3b658989a33d7454edb6c16b64
Daxow <voxizofficiel@gmail.com>
2026-04-03 11:07:31 +0200

    initialize database

---
+++ server/data/db.js
@@ -0,0 +1,13 @@
+const Database = require('better-sqlite3');
+
+const db = new Database('./database.db');
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS users (
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT    UNIQUE NOT NULL,
+    password TEXT    NOT NULL
+  )
+`);
+
+module.exports = db;
