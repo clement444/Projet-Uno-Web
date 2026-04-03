8df1aedcb74b343fe72ce5aaa4f1923a7e9a2ccb
Thomas <thomas.lessieu@ynov.com>
2026-04-03 10:28:33 +0200

    add(server): added file render

--- server/src/main.js
+++ server/src/main.js
@@ -1,12 +1,15 @@
 import express from "express";
 import Logger from "logpleaser";
+import path from "path";
 
 const app = express();
 const logger = Logger.create("main");
 const port = 3000;
 
+app.use("/src", express.static(path.join(__dirname, "../../client/public")));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "../../client/html/index.html"));
 });
 
 app.listen(port, () => {
