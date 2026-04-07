eda84cdc2becb3ec5c3a552f1dd27c7475a6e7ad
clement444 <clementmorere@gmail.com>
2026-04-07 11:31:48 +0200

    ADD (websocket) imports des handlers dans event_handler.js

---
+++ server/src/controllers/websocket/event_handler.js
@@ -0,0 +1,4 @@
+import { onJoinRoom } from "./events/join_room.js";
+import { onLeaveRoom } from "./events/leave_room.js";
+import { onPlayCard } from "./events/play_card.js";
+import { onDrawCard } from "./events/draw_card.js";
