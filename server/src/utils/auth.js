d8bbeab7f56357c0bcdafc89629187db9242cfd6
clement444 <clementmorere@gmail.com>
2026-04-08 12:45:22 +0200

    ADD (auth) implémentation de generate_token et verify_token avec JWT

--- server/src/utils/auth.js
+++ server/src/utils/auth.js
@@ -1,3 +1,11 @@
-export function generate_token() {
-  return "token";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "uno_secret_key";
+
+export function generate_token(user_id) {
+  return jwt.sign({ user_id }, JWT_SECRET, { expiresIn: "24h" });
+}
+
+export function verify_token(token) {
+  return jwt.verify(token, JWT_SECRET);
 }
