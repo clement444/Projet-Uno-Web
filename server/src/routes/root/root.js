34eaed8a7c6cb3df4ea850a423711859fc5d9304
Daxow <voxizofficiel@gmail.com>
2026-04-10 09:39:58 +0200

    fix routes

--- server/src/routes/root/root.js
+++ server/src/routes/root/root.js
@@ -10,16 +10,16 @@ export default () => {
 
   app.get("/lobby", (req, res) => {
     logger_http.info("[GET] /lobby");
-    res.sendFile(path.join(__dirname, "../../../../client/src/views/lobby.html"));
+    res.sendFile(path.join(__dirname, "../../../../client/src/views/lobby/lobby.html"));
   });
 
   app.get("/room", (req, res) => {
     logger_http.info("[GET] /room");
-    res.sendFile(path.join(__dirname, "../../../../client/src/views/room.html"));
+    res.sendFile(path.join(__dirname, "../../../../client/src/views/room/room.html"));
   });
 
   app.get("/game", (req, res) => {
     logger_http.info("[GET] /game");
-    res.sendFile(path.join(__dirname, "../../../../client/src/views/game.html"));
+    res.sendFile(path.join(__dirname, "../../../../client/src/views/game/game.html"));
   });
 };
