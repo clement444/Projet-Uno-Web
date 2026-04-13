53c93edec03cb1cd33ce0c1f2825eb71d8698728
clement444 <clementmorere@gmail.com>
2026-04-13 11:52:05 +0200

    FEAT (controllers/websocket/gameManager) registre en mémoire des parties actives

---
+++ server/src/controllers/websocket/gameManager.js
@@ -0,0 +1,17 @@
+import { Game } from "../../structures/game/game.js";
+
+const games = new Map();
+
+export function createGame(room_id, playerIds) {
+  const game = new Game(playerIds);
+  games.set(room_id, game);
+  return game;
+}
+
+export function getGame(room_id) {
+  return games.get(room_id) || null;
+}
+
+export function deleteGame(room_id) {
+  games.delete(room_id);
+}
