7c90d916f0bbe150281c726790aff468d031a3f1
clement444 <clementmorere@gmail.com>
2026-04-13 10:57:49 +0200

    FEAT (websocket/draw_card) validation room et joueur avant broadcast

--- server/src/controllers/websocket/events/draw_card.js
+++ server/src/controllers/websocket/events/draw_card.js
@@ -1,7 +1,25 @@
 import { broadcast } from "../broadcast.js";
+import { getRoomById } from "../../api/room.js";
 
 export function onDrawCard(_message, socket, wss) {
   const room_id = socket.room_id;
   const player_id = socket.user.id;
+
+  if (!room_id) {
+    socket.send(JSON.stringify({ error: "Pas dans une room" }));
+    return;
+  }
+
+  const room = getRoomById(room_id);
+  if (!room) {
+    socket.send(JSON.stringify({ error: "Room introuvable" }));
+    return;
+  }
+
+  if (!room.getPlayer(player_id)) {
+    socket.send(JSON.stringify({ error: "Pas dans cette room" }));
+    return;
+  }
+
   broadcast(wss, room_id, { type: "card_drawn", room_id, player_id });
 }
