fa720bbbeb4b2a90b1e79d980b55c5b3e44d4534
clement444 <clementmorere@gmail.com>
2026-04-13 10:39:38 +0200

    FIX (websocket/play_card) utiliser socket.user et socket.room_id au lieu du message client

--- server/src/controllers/websocket/events/play_card.js
+++ server/src/controllers/websocket/events/play_card.js
@@ -1,6 +1,8 @@
 import { broadcast } from "../broadcast.js";
 
 export function onPlayCard(message, socket, wss) {
-  const { room_id, player_id, card_id } = message;
+  const { card_id } = message;
+  const room_id = socket.room_id;
+  const player_id = socket.user.id;
   broadcast(wss, room_id, { type: "card_played", room_id, player_id, card_id });
 }
