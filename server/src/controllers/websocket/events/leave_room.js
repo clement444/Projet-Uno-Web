482b90ef9fe14db6d8969e5d74c9170f734905e6
clement444 <clementmorere@gmail.com>
2026-04-13 10:33:28 +0200

    FIX (websocket/leave_room) utiliser socket.user et socket.room_id au lieu du message client

--- server/src/controllers/websocket/events/leave_room.js
+++ server/src/controllers/websocket/events/leave_room.js
@@ -1,7 +1,9 @@
 import { broadcast } from "../broadcast.js";
 
-export function onLeaveRoom(message, socket, wss) {
-  const { room_id, player_id } = message;
-  broadcast(wss, room_id, { type: "player_left", room_id, player_id });
+export function onLeaveRoom(_message, socket, wss) {
+  const room_id = socket.room_id;
+  const player_id = socket.user.id;
+  const name = socket.user.username;
+  broadcast(wss, room_id, { type: "player_left", room_id, player_id, name });
   socket.room_id = null;
 }
