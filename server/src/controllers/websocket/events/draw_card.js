7981a2bb7de6fed21a0c8d8369abea9e55f7e0a7
clement444 <clementmorere@gmail.com>
2026-04-13 12:22:45 +0200

    FEAT (controllers/websocket/events/draw_card) piocher une carte et avancer le tour

--- server/src/controllers/websocket/events/draw_card.js
+++ server/src/controllers/websocket/events/draw_card.js
@@ -1,5 +1,6 @@
-import { broadcast } from "../broadcast.js";
+import { broadcast, sendToPlayer } from "../broadcast.js";
 import { getRoomById } from "../../api/room.js";
+import { getGame } from "../gameManager.js";
 
 export function onDrawCard(_message, socket, wss) {
   const room_id = socket.room_id;
@@ -16,10 +17,30 @@ export function onDrawCard(_message, socket, wss) {
     return;
   }
 
-  if (!room.getPlayer(player_id)) {
-    socket.send(JSON.stringify({ error: "Pas dans cette room" }));
+  const game = getGame(room_id);
+  if (!game) {
+    socket.send(JSON.stringify({ error: "Partie introuvable" }));
     return;
   }
 
-  broadcast(wss, room_id, { type: "card_drawn", room_id, player_id });
+  if (game.getCurrentPlayer() !== player_id) {
+    socket.send(JSON.stringify({ error: "Ce n'est pas ton tour" }));
+    return;
+  }
+
+  const drawn = game.drawCards(player_id, 1);
+  if (drawn.length === 0) {
+    socket.send(JSON.stringify({ error: "Plus de cartes dans la pioche" }));
+    return;
+  }
+
+  sendToPlayer(wss, player_id, {
+    type: "hand_update",
+    hand: game.getHand(player_id),
+    opponents: game.getOpponentState(player_id),
+  });
+  broadcast(wss, room_id, { type: "card_drawn", player_id });
+
+  game.nextTurn();
+  broadcast(wss, room_id, { type: "turn", player_id: game.getCurrentPlayer() });
 }
