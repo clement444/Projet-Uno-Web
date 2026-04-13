d475f803d0e40d1637c2fc9a7e6610147dbfd74d
clement444 <clementmorere@gmail.com>
2026-04-13 10:29:08 +0200

    FIX (websocket/join_room) utiliser socket.user au lieu du player_id client

--- server/src/controllers/websocket/events/join_room.js
+++ server/src/controllers/websocket/events/join_room.js
@@ -1,7 +1,9 @@
 import { broadcast } from "../broadcast.js";
 
 export function onJoinRoom(message, socket, wss) {
-  const { room_id, player_id, name } = message;
+  const { room_id } = message;
+  const player_id = socket.user.id;
+  const name = socket.user.username;
   socket.room_id = room_id;
   broadcast(wss, room_id, { type: "player_joined", room_id, player_id, name });
 }
