cdb204c4c9add0d69d8a8f013755cee4b704fe65
clement444 <clementmorere@gmail.com>
2026-04-13 11:05:51 +0200

    FEAT (structures/card) ajouter color, value, type et id dérivé

--- server/src/structures/game/card.js
+++ server/src/structures/game/card.js
@@ -1,7 +1,13 @@
 export class Card {
   id;
+  color;
+  value;
+  type;
 
-  constructor(id) {
-    this.id = id;
+  constructor(color, value) {
+    this.color = color;
+    this.value = value;
+    this.type = color === "wild" ? "wild" : isNaN(value) ? "action" : "number";
+    this.id = color === "wild" ? value : `${color}_${value}`;
   }
 }
