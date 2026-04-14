0a372bfb54dda8b2c4db57bc881642b8e6b8a42e
clement444 <clementmorere@gmail.com>
2026-04-14 14:56:41 +0200

    FEAT (controllers/websocket/broadcast) ajouter paramètre exclude_socket à broadcast

--- server/src/controllers/websocket/broadcast.js
+++ server/src/controllers/websocket/broadcast.js
@@ -1,7 +1,7 @@
-export function broadcast(wss, room_id, data) {
+export function broadcast(wss, room_id, data, exclude_socket = null) {
   const payload = JSON.stringify(data);
   wss.clients.forEach((client) => {
-    if (client.readyState === 1 && client.room_id === room_id) {
+    if (client.readyState === 1 && client.room_id === room_id && client !== exclude_socket) {
       client.send(payload);
     }
   });
