c4bd8de1c456954b31ccbe461a14cc54363417b1
clement444 <clementmorere@gmail.com>
2026-04-14 11:01:07 +0200

    FIX (client/public/js/game) afficher le nom de la room dans le header

--- client/public/js/game.js
+++ client/public/js/game.js
@@ -5,6 +5,7 @@ const roomId = localStorage.getItem("uno_room_id");
 if (!token || !roomId) window.location.href = "/";
 
 document.getElementById("current-player-name").textContent = username;
+document.getElementById("room-name").textContent = localStorage.getItem("uno_room_name") ?? "";
 
 const CARD_ASSETS = [
   "0","1","2","3","4","5","6","7","8","9",
