4d67db43de50994f621cd3d7faf71824e939ab92
clement444 <clementmorere@gmail.com>
2026-04-14 10:08:00 +0200

    FIX (controllers/websocket/router) supprimer joueur de room_players à la déconnexion et envoyer player_id

--- server/src/controllers/websocket/router.js
+++ server/src/controllers/websocket/router.js
@@ -1,5 +1,6 @@
 import { handleEvent } from "./event_handler.js";
 import { broadcast } from "./broadcast.js";
+import db from "../../utils/db.js";
 
 export function router(socket, wss) {
   socket.on("message", (raw) => {
@@ -15,8 +16,14 @@ export function router(socket, wss) {
   });
 
   socket.on("close", () => {
-    if (socket.room_id) {
-      broadcast(wss, socket.room_id, { type: "player_disconnected", room_id: socket.room_id });
+    if (socket.room_id && socket.user) {
+      db.prepare("DELETE FROM room_players WHERE room_id = ? AND user_id = ?")
+        .run(socket.room_id, socket.user.id);
+      broadcast(wss, socket.room_id, {
+        type: "player_disconnected",
+        room_id: socket.room_id,
+        player_id: socket.user.id,
+      });
     }
   });
 }
