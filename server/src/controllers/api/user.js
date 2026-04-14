621158e604e76b89b08f9dee37953a6c9991a4d2
Thomas <thomas.lessieu@ynov.com>
2026-04-14 09:59:51 +0200

    fix(user): added more logic to the base function

--- server/src/controllers/api/user.js
+++ server/src/controllers/api/user.js
@@ -1,86 +1,97 @@
 import db from "../../utils/db";
 import { generate_token } from "../../utils/auth";
 
-export async function createUser(req, res) {
-  const { username, password } = cleanCredentials(
-    req.body.username,
-    req.body.password,
+export async function createUser(username, password) {
+  const { clean_username, clean_password } = cleanCredentials(
+    username,
+    password,
   );
 
-  if (!username || !password)
-    return res
-      .status(400)
-      .json({ message: "Username and password is required." });
+  if (!clean_username || !clean_password)
+    throw new Error({
+      status_code: 400,
+      message: "Username and password is required.",
+    });
 
   if (username.length > 20)
-    return res.status(400).json({ message: "Username too long." });
+    throw new Error({ status_code: 400, message: "Username too long." });
 
-  const hashed = await Bun.password.hash(password);
+  const hashed = await Bun.password.hash(clean_password);
+
   try {
     const stmt = db.prepare(
       "INSERT INTO users (username, password_hash) VALUES (?, ?)",
     );
 
-    const result = stmt.run(username, hashed);
+    const result = stmt.run(clean_username, hashed);
     const token = generate_token(result.lastInsertRowid);
 
-    res.status(201).json({ token });
+    return { status_code: 201, token };
   } catch (err) {
     console.error("createUser error:", err);
 
     if (err.message?.includes("UNIQUE constraint failed")) {
-      return res.status(409).json({ message: "Username already taken." });
+      throw new Error({ status_code: 409, message: "Username already taken." });
     }
 
-    res.status(500).json({ message: "Cannot create account." });
+    throw new Error({ status_code: 500, message: "Cannot create account." });
   }
 }
 
-export async function loginUser(req, res) {
-  const { username, password } = req.body;
-  if (
-    !username ||
-    !password ||
-    username.trim() === "" ||
-    password.trim() === ""
-  )
-    return res
-      .status(400)
-      .json({ message: "Username and password is required." });
+export async function loginUser(username, password) {
+  const { clean_username, clean_password } = cleanCredentials(
+    username,
+    password,
+  );
+
+  if (!clean_username || !clean_password)
+    throw new Error({
+      status_code: 400,
+      message: "Username and password is required.",
+    });
 
   const user = db
     .prepare("SELECT * FROM users WHERE username = ?")
-    .get(username);
+    .get(clean_username);
   if (!user) {
-    return res.status(401).json({ message: "Wrong credentials." });
+    throw new Error({
+      status_code: 401,
+      message: "Wrong credentials.",
+    });
   }
 
-  const valid = await Bun.password.verify(password, user.password_hash);
+  const valid = await Bun.password.verify(clean_password, user.password_hash);
   if (!valid) {
-    return res.status(401).json({ message: "Wrong credentials." });
+    throw new Error({
+      status_code: 401,
+      message: "Wrong credentials.",
+    });
   }
 
   const token = generate_token(user.id);
-  res.json({ token });
+  return {
+    status_code: 200,
+    token,
+  };
 }
 
 export function removeUser() {}
 
 function cleanCredentials(username, password) {
-  const cleanupUsername = !username
+  const cleanup_username = !username
     ? null
     : username.trim() === ""
       ? null
       : username.trim();
 
-  const cleanupPassword = !password
+  const cleanup_password = !password
     ? null
     : password.trim() === ""
       ? null
       : password.trim();
 
   return {
-    username: cleanupUsername,
-    password: cleanupPassword,
+    username: cleanup_username,
+    password: cleanup_password,
   };
 }
--- server/src/routes/api/user.js
+++ server/src/routes/api/user.js
@@ -5,11 +5,23 @@ import { createUser, loginUser } from "../../controllers/api/user";
 export default () => {
   app.post("/api/register", (req, res) => {
     logger_http.info("[POST] /api/register");
-    createUser(req, res);
+
+    try {
+      const response = createUser(req.body.username, req.body.password);
+      res.status(response.status_code).json({ token: response.token });
+    } catch (e) {
+      res.status(e.status_code).json({ message: e.message });
+    }
   });
 
   app.post("/api/login", (req, res) => {
     logger_http.info("[POST] /api/login");
-    loginUser(req, res);
+
+    try {
+      const response = loginUser(req.body.username, req.body.password);
+      res.status(response.status_code).json({ token: response.token });
+    } catch (e) {
+      res.status(e.status_code).json({ message: e.message });
+    }
   });
 };
